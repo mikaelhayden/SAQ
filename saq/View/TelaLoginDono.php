3699489823e21462be0adeb42a8bb128b4fefe80
Mikael Hayden <62375520+mikaelhayden@users.noreply.github.com>
2022-03-17 18:42:25 -0300

    Update TelaLoginDono.php

--- saq/View/TelaLoginDono.php
+++ saq/View/TelaLoginDono.php
@@ -4,7 +4,9 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
     <title>SAQ-Login-Dono</title>
+
     <link rel="stylesheet" href="assets/css/bootstrap.min.css">
     <link rel="stylesheet" href="assets/css/nav_foot.css">
     <link rel="stylesheet" href="assets/css/style.css"> 
@@ -20,58 +22,60 @@
         </nav>
     </div>
 
-    <h1>Login Dono</h1>
-    <form method="POST">
-        <div class="form-login">
+    <div class="meio">
+        <h1>Login Dono</h1>
+        <form method="POST">
+            <div class="form-login">
             
-            <input type="email" id="inputEmail" name="Email_dono" placeholder="Entre com o email" required autofocus> <br>
+                <input type="email" id="inputEmail" name="Email_dono" placeholder="Entre com o email" required autofocus> <br>
             
-            <input type="password"  id="inputPassword" name="Senha_dono"  placeholder="Entre com a senha" required><br>
+                <input type="password"  id="inputPassword" name="Senha_dono"  placeholder="Entre com a senha" required><br>
             
-            <button class="buttonoptions" name="entrar" type="submit">Entrar</button>
-            <abbr title="Clique aqui para cadastrar-se"><a id="cadastrese" href="TelaCadastroDono.php">Cadastre-se</a></abbr> <br><br> 
+                <button class="buttonoptions" name="entrar" type="submit">Entrar</button>
+                <a id="cadastrese" href="TelaCadastroDono.php">Cadastre-se</a><br><br> 
        
-            <?php
-                //Conecta com os arquivos
-                require_once '../Controller/Conexao.php';
-                require_once '../Model/Dono.php';
+                <?php
+                    //Conecta com os arquivos
+                    require_once '../Controller/Conexao.php';
+                    require_once '../Model/Dono.php';
                 
-                //Instancias
-                $d = new Dono;
-                $conexao = new Conexao;
+                    //Instancias
+                    $d = new Dono;
+                    $conexao = new Conexao;
                 
-                if(isset($_POST['entrar'])) //Se o usuário clicar no botão "entrar"
-                {
-                    //php recebe os dados do formulário
-                    $email = addslashes($_POST["Email_dono"]);
-                    $senha = addslashes($_POST["Senha_dono"]);
-                    
-                    //verifica se esta tudo preenchido
-                    if(!empty($email) && !empty($senha))
+                    if(isset($_POST['entrar'])) //Se o usuário clicar no botão "entrar"
                     {
-                        $conexao->conectar("saq", "localhost", "root", ""); //Conecta com BD
-                        if($d -> logar($email, $senha)) //Se a função "logar" retornar "true"
+                        //php recebe os dados do formulário
+                        $email = addslashes($_POST["Email_dono"]);
+                        $senha = addslashes($_POST["Senha_dono"]);
+                    
+                        //verifica se esta tudo preenchido
+                        if(!empty($email) && !empty($senha))
                         {
-                            //session_start();
-                            setcookie("Email_dono", $email); //Cria o cookie da sessão com o email
-                            header("location: TelaDono.php"); //Redireciona para a Tela do Dono
-                        }
-                        else //Se a função "logar" retornar "false"
-                        {
-                            echo"<div id='erro'>Senha ou Email incorretos!</div>";
+                            $conexao->conectar("saq", "localhost", "root", ""); //Conecta com BD
+                            if($d -> logar($email, $senha)) //Se a função "logar" retornar "true"
+                            {
+                                //session_start();
+                                setcookie("Email_dono", $email); //Cria o cookie da sessão com o email
+                                header("location: TelaDono.php"); //Redireciona para a Tela do Dono
+                            }
+                            else //Se a função "logar" retornar "false"
+                            {
+                                echo"<div id='erro'>Senha ou Email incorretos!</div>";
+                            }
                         }
                     }
-                }
-            ?>
-            <a href="../index.html"> Voltar para index </a>
-        </div>        
-   </form>
-</body>
+                ?>
+            </div>        
+       </form>
+   </div>
 
-<footer class="rodape">
+    <footer class="rodape">
     <div class="internorodape"><br>
-    <p>APP Desenvolvido por</p><a href="https://github.com/WALTER-OBS-DEBUG" target="_blank" >Walter Jonas, Antony Gusmão e Mikael Hayden &copy; </a>
+    <p>APP Desenvolvido por</p><a href="https://github.com/WALTER-OBS-DEBUG">Walter Jonas, </a><a href="https://github.com/AntonyGuzma">Antony Gusmão, </a><a href="https://github.com/mikaelhayden">e Mikael Hayden &copy;</a>
+    <i class="bi bi-diamond-half"></i>
     <i class="bi bi-diamond-half"></i>
     </div>
 </footer>
+</body>
 </html>
