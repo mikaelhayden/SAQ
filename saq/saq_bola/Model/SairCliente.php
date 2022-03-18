c08f87a0e93577a220ba368f5d55dd5b486e6834
Antony _Gusmão <88747866+AntonyGuzma@users.noreply.github.com>
2022-03-18 19:56:18 -0300

    Delete saq/saq_bola directory

--- saq/saq_bola/Model/SairCliente.php
+++
@@ -1,5 +0,0 @@
-<?php
-	session_start();
-	unset($_SESSION['ID_Cliente']);
-	header("location: ../View/TelaLoginCliente.php");
-?>
--- saq/saq_bola/Model/SairFuncionario.php
+++
@@ -1,5 +0,0 @@
-<?php
-	session_start();
-	unset($_SESSION['ID_Funcionario']);
-	header("location: ../View/TelaLoginFuncionario.php");
-?>
